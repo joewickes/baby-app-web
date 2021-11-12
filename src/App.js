ad23307ec882a6e294781eb548b77ff82e98ab8b
joewickes <joe.c.wickes@gmail.com>
2021-11-12 18:19:13 -0500

    Make basic home page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Joe, hello</p>
+      <header>Header</header>
+      <section>
+        <div>LOGO</div>
+        <div>Current Stats</div>
+      </section>
+      <section>
+        <li>
+          <div>Blue Heading</div>
+          <div>Gray entry</div>
+          <div>White entry</div>
+        </li>
+      </section>
+      <section>
+        <footer>
+          <div></div>
+          <button>+</button>
+          <div></div>
+        </footer>
+      </section>
+      <section>AD SPACE</section>
     </div>
   );
 }
